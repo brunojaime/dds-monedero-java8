951967c8a3741755812de833859beb6333a6251c
Bruno <bruno.jaime@live.com.ar>
2024-05-03 16:05:14 -0300

    Long method en funcion poner

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -32,7 +32,7 @@ public class Cuenta {
     if (cuanto <= 0) {
       throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
     }
-  
+  // Long Method. getMovimientos().stream.filter.count()
     if (getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count() >= 3) {
       throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
     }
